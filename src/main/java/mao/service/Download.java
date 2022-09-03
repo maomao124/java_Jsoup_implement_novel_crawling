b590d69dda8a00ffcec41134a9513ae35be034c1
mao <1296193245@qq.com>
2022-09-03 21:37:53 +0800

    java_Jsoup_implement_novel_crawling

--- src/main/java/mao/service/Download.java
+++ src/main/java/mao/service/Download.java
@@ -72,6 +72,49 @@ public class Download
     }
 
     /**
+     * 缓存到文件，从startChapter章节开始缓存
+     *
+     * @param urlString    url字符串
+     * @param startChapter 开始章章节
+     * @throws IOException          ioexception
+     * @throws InterruptedException 中断异常
+     */
+    public static void toFile(String urlString, int startChapter) throws IOException, InterruptedException
+    {
+        //文字数量
+        long size = 0;
+        //获取目录和书名
+        Book book = CatalogueService.getCatalogue(urlString);
+        System.out.println("书名：" + book.getName());
+        List<Catalogue> list = book.getList();
+        System.out.println("一共" + list.size() + "章");
+        System.out.println("开始获取正文");
+        File file = new File(book.getName() + ".txt");
+        FileWriter fileWriter = new FileWriter(file);
+        for (int i = 0; i < list.size(); i++)
+        {
+            if (i < startChapter - 1)
+            {
+                continue;
+            }
+            Catalogue catalogue = list.get(i);
+            System.out.println("已缓存" + size + "字   " + "开始缓存：" + catalogue.getName());
+            Content content = ContentService.getContent(catalogue.getHref());
+            String s = content.getContent();
+            size = size + s.length();
+            fileWriter.write(content.getTitle() + "\n\n");
+            fileWriter.write(s);
+            fileWriter.write("\n\n\n\n");
+            fileWriter.flush();
+            //随机休眠
+            Thread.sleep(getIntRandom(200, 1000));
+        }
+        fileWriter.close();
+        System.out.println("缓存完成，本书一共" + size + "字");
+        Toolkit.getDefaultToolkit().beep();
+    }
+
+    /**
      * 缓存到文件，分散模式，一章一个文件
      *
      * @param urlString url字符串
@@ -117,9 +160,61 @@ public class Download
         Toolkit.getDefaultToolkit().beep();
     }
 
+    /**
+     * 缓存到文件，分散模式，一章一个文件，从startChapter章节开始
+     *
+     * @param urlString    url字符串
+     * @param startChapter 开始章节
+     * @throws IOException          IOException
+     * @throws InterruptedException 中断异常
+     */
+    public static void toFileDispersion(String urlString, int startChapter) throws IOException, InterruptedException
+    {
+        //文字数量
+        long size = 0;
+        //获取目录和书名
+        Book book = CatalogueService.getCatalogue(urlString);
+        System.out.println("书名：" + book.getName());
+        List<Catalogue> list = book.getList();
+        System.out.println("一共" + list.size() + "章");
+        System.out.println("开始获取正文");
+        File file1 = new File("./" + book.getName() + "/");
+        if (!file1.exists())
+        {
+            boolean b = file1.mkdirs();
+            if (!b)
+            {
+                System.out.println("目录创建失败！");
+                return;
+            }
+        }
+        for (int i = 0; i < list.size(); i++)
+        {
+            if (i < startChapter - 1)
+            {
+                continue;
+            }
+            Catalogue catalogue = list.get(i);
+            System.out.println("已缓存" + size + "字   " + "开始缓存：" + catalogue.getName());
+            Content content = ContentService.getContent(catalogue.getHref());
+            File file = new File("./" + book.getName() + "/" + content.getTitle() + ".txt");
+            FileWriter fileWriter = new FileWriter(file);
+            String s = content.getContent();
+            size = size + s.length();
+            fileWriter.write(s);
+            fileWriter.flush();
+            fileWriter.close();
+            //随机休眠
+            Thread.sleep(getIntRandom(200, 1000));
+        }
+
+        System.out.println("缓存完成，本书一共" + size + "字");
+        Toolkit.getDefaultToolkit().beep();
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        //toFile("http://www.biqu5200.net/52_52542/");
-        toFileDispersion("http://www.biqu5200.net/52_52542/");
+        //toFile("http://www.biqu5200.net/52_52542/", 494);
+        toFileDispersion("http://www.biqu5200.net/52_52542/", 211);
     }
 }
